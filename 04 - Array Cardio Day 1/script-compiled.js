b84a8e30246995788d65e78f4bf90d20b9160891
Devon Jue <devon.jue@blackswan.com>
2017-03-13 22:17:17 -0700

    array cardio day 1 added

---
+++ 04 - Array Cardio Day 1/script-compiled.js
@@ -0,0 +1,88 @@
+'use strict';
+
+// Get your shorts on - this is an array workout!
+// ## Array Cardio Day 1
+
+// Some data we can work with
+
+var inventors = [{ first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 }, { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 }, { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 }, { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 }, { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 }, { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 }, { first: 'Max', last: 'Planck', year: 1858, passed: 1947 }, { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 }, { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 }, { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 }, { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 }, { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }];
+
+var people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
+
+// Array.prototype.filter()
+// 1. Filter the list of inventors for those who were born in the 1500's
+var oldInventors = inventors.filter(function (inventor) {
+    return inventor.year >= 1500 && inventor.year < 1600;
+});
+
+console.log(oldInventors);
+
+// Array.prototype.map()
+// 2. Give us an array of the inventors' first and last names
+var nameInventors = inventors.map(function (inventor) {
+    return inventor.first + ' ' + inventor.last;
+});
+
+console.log(nameInventors);
+// Array.prototype.sort()
+// 3. Sort the inventors by birthdate, oldest to youngest
+
+var sortedInventors = inventors.sort(function (a, b) {
+    return a.year < b.year ? -1 : 1;
+});
+
+console.log(sortedInventors);
+// Array.prototype.reduce()
+// 4. How many years did all the inventors live?
+
+var totalYears = inventors.reduce(function (years, inventor) {
+    return years + (inventor.passed - inventor.year);
+}, 0);
+
+console.log(totalYears);
+
+// 5. Sort the inventors by years lived
+
+// const oldInventorRank = inventors.sort((a, b) => {
+//     return (a.passed - a.year) < (b.passed - b.year) ? -1 : 1;
+// });
+
+var oldInventorRank = inventors.map(function (inventor) {
+    inventor.yearsLived = inventor.passed - inventor.year;
+    return inventor;
+}).sort(function (a, b) {
+    return a.yearsLived < b.yearsLived ? 1 : -1;
+});
+
+console.log(oldInventorRank);
+
+// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+var links = document.querySelectorAll(".mw-category a");
+var linksArray = Array.from(links);
+var deArray = linksArray.map(function (link) {
+    return link.textContent;
+}).filter(function (linkText) {
+    return linkText.includes("de");
+});
+console.log(deArray);
+
+// 7. sort Exercise
+// Sort the people alphabetically by last name
+var alphabeticLastName = people.sort(function (a, b) {
+    var aLastName = a.split(", ")[0];
+    var bLastName = b.split(", ")[0];
+
+    return aLastName < bLastName ? -1 : 1;
+});
+console.log(alphabeticLastName);
+
+// 8. Reduce Exercise
+// Sum up the instances of each of these
+var data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck'];
+var sum = data.reduce(function (sumMap, word) {
+    if (!sumMap.hasOwnProperty(word)) sumMap[word] = 1;else sumMap[word]++;
+    return sumMap;
+}, {});
+
+console.log(sum);
