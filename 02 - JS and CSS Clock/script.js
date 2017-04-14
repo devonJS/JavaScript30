26dfbdfb82554c5d6b04d12af3580d3c9b426755
Devon Jue <devon.jue@blackswan.com>
2017-04-13 23:04:50 -0700

    added JS and CSS clock challenge

---
+++ 02 - JS and CSS Clock/script.js
@@ -0,0 +1,27 @@
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+var secondIncrementer = 0;
+var minuteIncrementer = 0;
+var hourIncrementer = 0;
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();  
+  if(seconds === 0) secondIncrementer++;
+  const secondsDegrees = ((seconds / 60) * 360);
+  
+  const minutes = now.getMinutes();
+  if(minutes === 0) minuteIncrementer++;
+  const minutesDegrees = ((minutes / 60) * 360);
+  
+  const hours = now.getHours();
+  if(hours === 0) hourIncrementer++;
+  const hoursDegrees = hours <= 12 ? ((hours / 12) * 360) : (((hours % 12) / 12) * 360);
+  
+  secondHand.style.transform = `rotate(${90 + (360 * secondIncrementer) + secondsDegrees}deg)`;
+  minuteHand.style.transform = `rotate(${90 + (360 * minuteIncrementer) + minutesDegrees}deg)`;
+  hourHand.style.transform = `rotate(${90 + (360 * hourIncrementer) + hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
